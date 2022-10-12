b4c82dd5e64891ad455d2de724263927aa5454e4
Lionis Emmanouil Georgios(Akis) <akislionis@gmail.com>
2022-10-12 19:12:28 +0300

    Polygon example with visualization

--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
-#include <iostream>
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
-#include <sstream>
-
-typedef CGAL::Exact_predicates_exact_constructions_kernel Kernel;
-typedef Kernel::Point_2 Point_2;
-
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Polygon_2.h>
+#include <list>
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Point_2<K> Point;
+typedef CGAL::Polygon_2<K> Polygon_2;
+typedef Polygon_2::Vertex_iterator VertexIterator;
+typedef Polygon_2::Edge_const_iterator EdgeIterator;
 int main()
-{
-    Point_2 p, q, r;
-    {
-        std::istringstream input("0 0.3 1 0.6 2 0.9");
-        input >> p >> q >> r;
-        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not"
-        " collinear\n");
-    }
-    {
-        std::istringstream input("0 0.4 1 0.8 2 1.2");
-        input >> p >> q >> r;
-        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not"
-        " collinear\n");
-    }
-    {
-        Point_2 p(0,0), q(1, 1), r(2, 2);
-        std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not"
-        "collinear\n");
-    }
-    return 0;
+{ 
+  // create a polygon and put some points in it
+  Polygon_2 p;
+  p.push_back(Point(0,0));
+  p.push_back(Point(4,0));
+  p.push_back(Point(4,4));
+  p.push_back(Point(2,2));
+  p.push_back(Point(0,4));
+  std::cout<< * (VertexIterator)p.top_vertex()<<std::endl;
+  VertexIterator x=p.top_vertex(); 
+  p.insert(x,Point(5,5));
+  
+  int n=0;
+  for (VertexIterator vi = p.vertices_begin(); vi != p.vertices_end(); ++vi)
+    std::cout << "vertex " << n++ << " = " << *vi << std::endl;
+  std::cout << std::endl;
+  n=0;
+  for (EdgeIterator ei = p.edges_begin(); ei != p.edges_end(); ++ei)
+    std::cout << "edge " << n++ << " = " << *ei << std::endl;
+  return 0;
 }
