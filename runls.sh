36e7d37bd607b090e4decc81e8f71c8c4d5a6259
elenaliar <sdi1900100@di.uoa.gr>
2022-11-30 19:28:17 +0200

    final run

--- runls.sh
+++ runls.sh
@@ -3,4 +3,4 @@
 cgal_create_CMakeLists -s main
 cmake -DCGAL_DIR=/usr/lib/CGAL -CMAKE_BUILD_TYPE=RELEASE
 make
-./main -i /Users/elenaliarou/Documents/term7/project/instances/data/images/euro-night-0000200.instance -o output.txt -algorithm local_search -L 10 -max -threshold 10000 -algorithm2 incremental -edgeselection 2 -initilization 1a
+./main -i ./instances/data/images/euro-night-0000200.instance -o output.txt -algorithm local_search -L 10 -max -threshold 10000 -algorithm2 incremental -edgeselection 2 -initilization 1a
