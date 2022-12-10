784c401d435f0fa7c0f7f6d889cc47de14f48584
Lionis Emmanouil Georgios(Akis) <akislionis@gmail.com>
2022-12-10 15:04:24 +0200

    run change

--- run.sh
+++ run.sh
@@ -9,9 +9,4 @@ then
 elif [ $1 -eq 2 ]
 then
     ./main -i ./instances/data/images/euro-night-0000100.instance -o output3-1.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 incremental  -edgeselection 1 -initialization 1a 
-    ./main -i ./instances/data/images/euro-night-0000100.instance -o output3-2.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
-    ./main -i ./instances/data/images/euro-night-0000400.instance -o output3-3.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 incremental  -edgeselection 1 -initialization 1a 
-    ./main -i ./instances/data/images/euro-night-0000400.instance -o output3-4.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
-    ./main -i ./instances/data/uniform/uniform-0000400-1.instance -o output3-5.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
-    ./main -i ./instances/data/images/euro-night-0001000.instance -o output1-6.txt -algorithm simulated_annealing -L 1000 -max -annealing local -algorithm2 convex_hull  -edgeselection 1  
 fi
