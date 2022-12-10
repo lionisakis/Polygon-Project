4c4aeaf0fc28c6a21955a9e6105df9fb61a63c5b
Lionis Emmanouil Georgios(Akis) <akislionis@gmail.com>
2022-12-10 15:02:34 +0200

    final commit

--- run.sh
+++ run.sh
@@ -8,5 +8,10 @@ then
     ./main -i ./instances/data/images/euro-night-0000100.instance -o output.txt -algorithm local_search -L 10 -max -threshold 1 -algorithm2 incremental -edgeselection 3 -initialization 1a
 elif [ $1 -eq 2 ]
 then
-    ./main -i ./instances/data/uniform/uniform-0000400-1.instance -o output.txt -algorithm simulated_annealing -L 1000 -min -annealing subdivision -algorithm2 convex_hull  -edgeselection 1  -m 100
+    ./main -i ./instances/data/images/euro-night-0000100.instance -o output3-1.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 incremental  -edgeselection 1 -initialization 1a 
+    ./main -i ./instances/data/images/euro-night-0000100.instance -o output3-2.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
+    ./main -i ./instances/data/images/euro-night-0000400.instance -o output3-3.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 incremental  -edgeselection 1 -initialization 1a 
+    ./main -i ./instances/data/images/euro-night-0000400.instance -o output3-4.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
+    ./main -i ./instances/data/uniform/uniform-0000400-1.instance -o output3-5.txt -algorithm simulated_annealing -L 1000 -max -annealing subdivision -algorithm2 convex_hull  -edgeselection 1 
+    ./main -i ./instances/data/images/euro-night-0001000.instance -o output1-6.txt -algorithm simulated_annealing -L 1000 -max -annealing local -algorithm2 convex_hull  -edgeselection 1  
 fi
